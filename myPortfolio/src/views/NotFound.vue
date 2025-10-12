c79e0433fbf1f9cf4eb777daadfdb69ec21b4e61
DeKeyserThyssa <thyssa.dekeyser@hotmail.com>
2025-10-12 17:45:53 +0200

    update not found page

--- myPortfolio/src/views/NotFound.vue
+++ myPortfolio/src/views/NotFound.vue
@@ -10,7 +10,7 @@
     </p>
     <RouterLink
         to="/"
-        class="px-4 py-2 bg-[#718355] text-white rounded-lg hover:bg-[#5e6e4f]"
+        class="px-4 py-2 bg-[#E6EEDB] text-black flex items-center gap-2 hover:bg-[#d9e4c9] rounded-lg"
     >
       Go Home
     </RouterLink>
